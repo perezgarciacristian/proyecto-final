f1d335759a585cc729e72eadb3427012330bf35a
perezgarciacristian <85454017+perezgarciacristian@users.noreply.github.com>
2022-10-13 16:19:42 -0600

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -7,6 +7,8 @@ export default defineConfig({
             input: [
                 'resources/css/app.css',
                 'resources/js/app.js',
+                'resources/css/materialize.css',  
+                'resources/js/materialize.js'
             ],
             refresh: [
                 ...refreshPaths,

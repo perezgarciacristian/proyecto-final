9c98a851113e118d5e51d35e8d4ac55200e2ec38
perezgarciacristian <cristianperezgarcia123@gmail.com>
2022-12-03 22:48:37 -0700

    proyecto final casi terminado

--- app/Models/Seller.php
+++ app/Models/Seller.php
@@ -4,10 +4,12 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Seller extends Model
 {
     use HasFactory;
+    use SoftDeletes;
     protected $fillable = ['Nombre', 'user_id', 'Genero',];
     /*public $timestamps = false;*/
 

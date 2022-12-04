8cb537c0da8bc00b87242055b403adf7c98e5c1f
Fratroy0308 <raul.maravilla9279@alumnos.udg.mx>
2022-12-04 15:18:46 -0600

    crear vendedor

--- resources/views/Seller/sellerCreate.blade.php
+++ resources/views/Seller/sellerCreate.blade.php
@@ -1,29 +1,31 @@
 <x-template titulo='Formulario Vendedor'>
 
 
-   <form action="/seller" method="post">
+   <form action="/seller" method="post" enctype="multipart/form-data" class="container">
     @csrf
 
-    <div>
-    <label for="Nombre">Nombre:</label>
-    <input type="text"  name="Nombre">
+    <div class="form-group">
+    <label for="Nombre" class="form-label">Nombre:</label>
+    <input type="text"  name="Nombre" class="form-control">
 </div>
 
     
-    <div>
+    <div class="form-group">
     <p>Genero:
-        <input name="Genero" type="radio" value="M">M
-        <input  name="Genero"type="radio" value="F">F
+        <input name="Genero" type="radio" value="M" class="form-check-input">M
+        <input  name="Genero"type="radio" value="F" class="form-check-input">F
       </p>
 </div>
-   <select name="user_id" id="user_id">
+   <select name="user_id" id="user_id" class="form-select">
     @foreach($users as $user)
        <option   value="{{$user->id}}">{{$user->name}}</option>
     @endforeach
 
    </select>
   
-  <input type="submit" value="Registrar"> 
+   <div class="text-center my-2">
+      <input class="btn btn-success" type="submit" value="Registrar">
+   </div>
  
 </form>
 

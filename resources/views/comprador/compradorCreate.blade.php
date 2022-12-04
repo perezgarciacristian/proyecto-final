adf2eec91d97e635fba9f7f1d23adbb313d8a908
Fratroy0308 <raul.maravilla9279@alumnos.udg.mx>
2022-12-04 06:27:07 -0600

    crear comprador

--- resources/views/comprador/compradorCreate.blade.php
+++ resources/views/comprador/compradorCreate.blade.php
@@ -1,15 +1,15 @@
 <x-template titulo='Formulario Comprador'>
 
 
-    <form action="/buyer" method="post">
+    <form action="/buyer" method="post" enctype="multipart/form-data" class="container">
         @csrf
 
-        <div>
-            <label for="Nombre">Nombre:</label>
+        <div class="form-group">
+            <label for="Nombre" class="form-label">Nombre:</label>
             <input type="text" name="Nombre">
         </div>
 
-        <div>
+        <div class="form-group">
             <p>Edad:
                 <input type="radio" name="Edad" value="menor"> Menor
                 <input type="radio" name="Edad" value="adulto" required> Adulto
@@ -17,9 +17,9 @@
         </div>
 
 
-        <div>
-            <label for="Mascota">Mascota:</label>
-            <select name="Mascota" id="identified">
+        <div class="form-group">
+            <label for="Mascota" class="form-label">Mascota:</label>
+            <select name="Mascota" id="identified" class="form-select">
                 <option value="Perro">Perro</option>
                 <option value="Gato">Gato</option>
                 <option value="Pez">Pez</option>
@@ -27,7 +27,7 @@
             </select>
         </div>
 
-        <select name="user_id" id="user_id">
+        <select name="user_id" id="user_id" class="form-select">
             @foreach ($users as $user)
                 <option value="{{ $user->id }}">{{ $user->name }}</option>
             @endforeach
@@ -35,8 +35,7 @@
         </select>
 
 
-
-        <input type="submit" value="Registrar">
+        <div class="text-center my-2"><input class="btn btn-success" type="submit" value="Registrar"></div>
 
     </form>
 </x-template>

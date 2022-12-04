7704369f25b0baee20b121892d99a307f7731aa8
GHOST202010 <robin202010@hotmail.com>
2022-12-04 14:24:14 -0600

    Cambios vista comprador.

--- resources/views/comprador/compradorCreate.blade.php
+++ resources/views/comprador/compradorCreate.blade.php
@@ -1,4 +1,4 @@
-<x-template titulo='Formulario Comprador'>
+<x-template titulo='Crear Comprador'>
 
 
     <form action="/buyer" method="post" enctype="multipart/form-data" class="container">
@@ -6,37 +6,37 @@
 
         <div class="form-group">
             <label for="Nombre" class="form-label">Nombre:</label>
-            <input type="text" name="Nombre">
+            <input type="text" name="Nombre" value="{{ old('Nombre') ?? '' }}" required>
+            @error('Nombre')
+                <i>Por favor escriba un nombre</i>
+            @enderror
         </div>
 
         <div class="form-group">
-            <p>Edad:
-                <input type="radio" name="Edad" value="menor"> Menor
-                <input type="radio" name="Edad" value="adulto" required> Adulto
-            </p>
+            <label>Edad:</label>
+            <input class="form-check-input" type="radio" name="Edad" value="menor" required
+                {{ old('Edad') == 'menor' ? 'checked' : '' }}>
+            <label for=""> Menor</label>
+            <input class="form-check-input" type="radio" name="Edad" value="adulto"
+                {{ old('Edad') == 'adulto' ? 'checked' : '' }}>
+            <label for="">Adulto</label>
+            @error('Edad')
+                <i>Por favor seleccione una edad</i>
+            @enderror
         </div>
-
-
         <div class="form-group">
             <label for="Mascota" class="form-label">Mascota:</label>
-            <select name="Mascota" id="identified" class="form-select">
-                <option value="Perro">Perro</option>
-                <option value="Gato">Gato</option>
-                <option value="Pez">Pez</option>
-                <option value="Otra">Otra</option>
+            <select name="Mascota" id="identified" class="form-select" required>
+                <option selected disabled>Elegir animal</option>
+                <option value="Perro" {{ old('Mascota') == 'Perro' ? 'selected' : '' }}>Perro</option>
+                <option value="Gato" {{ old('Mascota') == 'Gato' ? 'selected' : '' }}>Gato</option>
+                <option value="Pez" {{ old('Mascota') == 'Pez' ? 'selected' : '' }}>Pez</option>
+                <option value="Otra" {{ old('Mascota') == 'Otra' ? 'selected' : '' }}>Otra</option>
             </select>
+            @error('Mascota')
+                <i>Por favor seleccione el tipo de mascota</i>
+            @enderror
         </div>
-
-        <select name="user_id" id="user_id" class="form-select">
-            @foreach ($users as $user)
-                <option value="{{ $user->id }}">{{ $user->name }}</option>
-            @endforeach
-
-        </select>
-
-
         <div class="text-center my-2"><input class="btn btn-success" type="submit" value="Registrar"></div>
-
     </form>
 </x-template>
-
